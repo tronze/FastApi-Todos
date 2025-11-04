42e3c8467dd71a95fb12fd946851a578e2ddef0b
Tronze <0183535663@naver.com>
2025-11-04 23:14:01 +0900

    fix: 필요없는 comment 제거

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ def load_todos():
 
 # JSON 파일에 To-Do 항목 저장
 def save_todos(todos):
-    # 상위 디렉터리가 없으면 생성 (예: TODO_FILE=/data/todo.json)
+    # 상위 디렉터리가 없으면 생성
     os.makedirs(os.path.dirname(TODO_FILE), exist_ok=True)
     with open(TODO_FILE, "w") as file:
         json.dump(todos, file, indent=4)
