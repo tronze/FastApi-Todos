83045acfa80b344fe65b9797c909b3fdea199be0
Tronze <0183535663@naver.com>
2025-12-02 14:04:17 +0900

    chore: - Loki 셋업

--- src/main.py
+++ src/main.py
@@ -3,7 +3,13 @@ import os
 import time
 from typing import Annotated
 
-from fastapi import FastAPI, HTTPException, Request, Form
+import logging
+from multiprocessing import Queue
+from os import getenv
+
+from logging_loki import LokiQueueHandler
+
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -12,10 +18,40 @@ from starlette import status
 
 from prometheus_fastapi_instrumentator import Instrumentator
 
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 app = FastAPI()
 
 Instrumentator().instrument(app).expose(app, endpoint="/metrics")
+
+loki_logs_handler = LokiQueueHandler(
+    Queue(-1),
+    url=getenv("LOKI_ENDPOINT"),
+    tags={"application": "fastapi"},
+    version="1",
+)
+
+# Custom access logger (ignore Uvicorn's default logging)
+custom_logger = logging.getLogger("custom.access")
+custom_logger.setLevel(logging.INFO)
+
+# Add Loki handler (assuming `loki_logs_handler` is correctly configured)
+custom_logger.addHandler(loki_logs_handler)
+
+async def log_requests(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    duration = time.time() - start_time  # Compute response time
+
+    log_message = (
+        f'{request.client.host} - "{request.method} {request.url.path} HTTP/1.1" {response.status_code} {duration:.3f}s'
+    )
+
+    # **Only log if duration exists**
+    if duration:
+        custom_logger.info(log_message)
+
+    return response
 
 # Jinja Template Loading을 위한 초기화.
 templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
